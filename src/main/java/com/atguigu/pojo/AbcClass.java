adffa11c32193a9e6f701d588a2e61cd87589623
朱振林 <2510508776@qq.com>
2021-02-02 20:20:37 +0800

    添加一句语句

--- src/main/java/com/atguigu/pojo/AbcClass.java
+++ src/main/java/com/atguigu/pojo/AbcClass.java
@@ -3,5 +3,6 @@ package com.atguigu.pojo;
 public class AbcClass {
     public static void main(String[] args) {
         System.out.println("111111dev");
+        System.out.println("我就是我，颜色不一样的烟火");
     }
 }
